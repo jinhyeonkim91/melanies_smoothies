6d7813308194f153cff069f5d7f2272e66b0d1bb
jinhyeonkim91 <61487880+jinhyeonkim91@users.noreply.github.com>
2024-10-01 12:23:40 +1000

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-tv_df = st.dataframe(data=fruityvice_response.json()), use_container_width=True)
+tv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
